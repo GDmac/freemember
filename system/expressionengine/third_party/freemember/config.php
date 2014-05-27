199ff1021a9362dc89986499587b46ba3961fa58
Adrian Macneil <adrian@adrianmacneil.com>
2014-05-26 23:07:10 -0700

    Don't check version inside installer, seems to break on some installs

--- system/expressionengine/third_party/freemember/config.php
+++ system/expressionengine/third_party/freemember/config.php
@@ -1,6 +1,7 @@
 <?php
 
-if (defined('APP_VER') && version_compare(APP_VER, '2.6.0', '<')) {
+// don't check version inside installer, seems to break on some installs
+if (defined('APP_VER') && !defined('EE_APPPATH') && version_compare(APP_VER, '2.6.0', '<')) {
     show_error('FreeMember requires ExpressionEngine version 2.6+, you have '.APP_VER);
 }
 
